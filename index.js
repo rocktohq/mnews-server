0af6f72c8f9619c4679a98a6dbf1c2125426cfee
rocktohq <itzmonir@gmail.com>
2023-11-25 04:26:36 +0600

    MongoDB Configured

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -11,6 +13,33 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("mNews Server is running...");
 });
+
+// MongoDB URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@usermanagement.n4peacj.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // * Collections
+    const articleCollection = client.db("mNews").collection("articles");
+
+    // * Get APIs
+    // Get Articles
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 // Listener
 app.listen(port, () => {
